6c1a35b32451d51ec6cf983f90ae0e6636c882fb
Fran12302 <156135106+Fran12302@users.noreply.github.com>
2024-01-15 19:05:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class DieselEngine{
+
+
+
+        }
+
+    public class PetrolEngine
+    {
+
+    }
+
+
+    public class Car
+    {
+        DieselEngine dieselEngine;
+
+        public Car()
+        {
+            dieselEngine = new DieselEngine();
+        }
+    }
+
+
+    // Radimo čvrstu spregu klase Car s klasom DieseEgine jer ne ovisimo o apstrakciji
+}
+
